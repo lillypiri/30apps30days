80187344a9e3a8f895cba1b351cb97dcfe6fa2a7
Lilly Piri <lilly@lillypiri.com>
2017-06-02 13:56:47 +1000

    pocky kitty added, edited app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import pockybebe from './images/blinky.gif';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+
+          <img src={pockybebe} className="pocky" alt="pocky" />
+          <h2>30 apps in 30 days</h2>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         We're going to need SNACKS for this!
         </p>
       </div>
     );
