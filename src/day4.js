4540d896148ccf0e86a91007605e3c4f46e3562a
Lilly Piri <lilly@lillypiri.com>
2017-06-05 17:59:32 +1000

    speech bubble added

--- src/day4.js
+++ src/day4.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import pepper from './images/pepper.gif';
+import bubble from './images/speechbubble.png';
 import './day4.css';
 
 class Four extends Component {
     render() {
         return(
             <div>
-                <div className="header">
-                    test
+                <div className="bubble">
+                    <img src={bubble} alt="bubble" />
                 </div>
                 <div className="pepper">
                     <img src={pepper} className="images" alt="pepper" />
