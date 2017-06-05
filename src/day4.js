2257ad9ba9e4635da4f625b858294d8dc2e97d1b
Lilly Piri <lilly@lillypiri.com>
2017-06-05 21:31:04 +1000

    set click event

--- src/day4.js
+++ src/day4.js
@@ -4,13 +4,38 @@ import bubble from './images/speechbubble.png';
 import './day4.css';
 
 class Four extends Component {
+    constructor (props) {
+        super(props);
+            this.state = {
+                isRisen: false,
+            }
+    }
+
+    onPepperClick () {
+        this.setState ({
+            isRisen: true
+        });
+    }
+    
     render() {
         return(
             <div>
+
                 <div className="pepper">
-                    <img src={pepper} className="images" alt="pepper" />
+                    <img src={pepper} className="images" alt="pepper" onClick={e => this.onPepperClick()}/>
                         <div className="bubble">
                             <img src={bubble} alt="bubble" />
+                            <div className="pepper-words">
+
+                                {!this.state.isRisen &&
+                                    "Hello, my name is Pepper. The world's cutest humanoid robot."
+                                }
+
+                                {this.state.isRisen &&
+                                    "You have angered me."
+                                }
+
+                            </div>
                         </div>
                 </div>
 
