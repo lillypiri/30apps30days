6f7bdebe4b1bb42eb03dfe15e63ade3974a9e728
Lilly Piri <lilly@lillypiri.com>
2017-06-06 14:58:13 +1000

    switch cases added

--- src/day5.js
+++ src/day5.js
@@ -12,11 +12,11 @@ class Five extends Component {
     }
 
     onButtonClick () {
-        var newmood = Math.floor(Math.random()*3)
+        var newmood = Math.floor(Math.random()*7)
 
         while (newmood === this.state.mood) {
-            newmood = Math.floor(Math.random()*3)
-        }  
+            newmood = Math.floor(Math.random()*7)
+        }
         this.setState ({
             mood: newmood
         });
@@ -56,6 +56,87 @@ class Five extends Component {
                 </div>
             )
             break;
+
+            case 2:
+            screen = (
+                <div>
+                    <div>☆                 ✧ ☆    ✧✧</div>
+                    <div>     ^,,,,✿,^ </div>
+                    <div>   ☆     (ﾐⓛ∇ⓛﾐ)   ✧ </div>
+                    <div>    (###,,--,,##)</div>
+                    <div>      (###,, , , ,,##)</div>
+                    <div>      ✿..(, (,,), ,(,,) ,,)~~~~</div>
+                    <div>   </div>
+                    <div>  MEOW MEOW!</div>
+                    <div> [I enjoy chasing the red dot!]</div>
+                </div>
+            )
+            break;
+
+            case 3:
+            screen = (
+                <div>
+                    <div>☆                 ✧ ☆    ✧✧</div>
+                    <div>     ^,,,,✿,^ </div>
+                    <div>      *:･ﾟ✧(ﾐ✪ᆺ✪ﾐ)*:･ﾟ✧ </div>
+                    <div>    (###,,--,,##)</div>
+                    <div>      (###,, , , ,,##)</div>
+                    <div>      ✿..(, (,,), ,(,,) ,,)~~~~</div>
+                    <div>   </div>
+                    <div>  MEOW MEOW!</div>
+                    <div> [Food is so dreamy.]</div>
+                </div>
+            )
+            break;
+
+            case 4:
+            screen = (
+                <div>
+                    <div>☆                 ✧ ☆    ✧✧</div>
+                    <div>     ^,,,,✿,^ </div>
+                    <div>      *:･ﾟ✧(ﾐ♡ᆺ♡ﾐ)*:･ﾟ✧ </div>
+                    <div>    (###,,--,,##)</div>
+                    <div>      (###,, , , ,,##)</div>
+                    <div>      ✿..(, (,,), ,(,,) ,,)~~~~</div>
+                    <div>   </div>
+                    <div>  meoooowwwwza!</div>
+                    <div> [Nathan is so handsome.]</div>
+                </div>
+            )
+            break;
+
+            case 5:
+            screen = (
+                <div>
+                    <div>☆                 z Z    ✧✧</div>
+                    <div>     ^,,,,✿,^ </div>
+                    <div>      *:･ﾟ✧(ﾐ✖ᆺ✖ﾐ)*:･ﾟ✧ </div>
+                    <div>    (###,,--,,##)</div>
+                    <div>      (###,, , , ,,##)</div>
+                    <div>      ✿..(, (,,), ,(,,) ,,)~~~~</div>
+                    <div>   </div>
+                    <div>  meow..</div>
+                    <div> [I need a nap..]</div>
+                </div>
+            )
+            break;
+
+            case 6:
+            screen = (
+                <div>
+                    <div>☆                 . ?    ✧?</div>
+                    <div>     ^,,,,✿,^ </div>
+                    <div>      *:･ﾟ✧(ﾐචᆺචﾐ)*:･ﾟ✧ </div>
+                    <div>    (###,,--,,##)</div>
+                    <div>      (###,, , , ,,##)</div>
+                    <div>      ✿..(, (,,), ,(,,) ,,)~~~~</div>
+                    <div>   </div>
+                    <div>  Mrow.</div>
+                    <div> [To be? Or not to be?]</div>
+                </div>
+                )
+
+
         }
         return (
             <section className="cube">
