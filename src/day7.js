c449ec18671a8e002ccb9b5c0099300726a35839
Lilly Piri <lilly@lillypiri.com>
2017-06-08 18:29:41 +1000

    css & states are working

--- src/day7.js
+++ src/day7.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
-import painting from './images/piri_painting.jpg';
+import painting from './images/piri_painting.gif';
+import out from './images/piri_painting_out.gif';
 import './day7.css'
 
 
 class Painting extends Component {
+    constructor(props) {
+        super (props);
+        this.state = {
+            isOut: false
+        };
+    }
+
+
     render () {
         return (
             <div>
                 <div className="painting">
-                    <img src={painting} alt="painting" />
+                    <div className="frame">
+                        {!this.state.isOut &&
+                            <img src={painting} alt="painting" />
+                        }
+                        {this.state.isOut &&
+                            <img src={out} alt="out" />
+                        }
+                    </div>
+                </div>
+
+                <div className="plaque bronze" onClick={e => this.setState({ isOut: true })}>
+                    make a wish
                 </div>
             </div>
         )
