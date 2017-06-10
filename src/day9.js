e2eedb54017384f0c34b2d2e26d289dd08705068
Lilly Piri <lilly@lillypiri.com>
2017-06-10 12:16:46 +1000

    created form using controlled components

--- src/day9.js
+++ src/day9.js
@@ -2,9 +2,41 @@ import React, {Component} from 'react';
 import './day9.css'
 
 class Nine extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'Aries'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Your horrorscope for ' + this.state.value + ' is: Watch out for raining ice cream!');
+        event.preventDefault();
+    }
     render() {
         return (
             <div>
+                <div className="form-nine">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <div>
+                                Choose your star sign:
+                            </div>
+                            <select value={this.state.value} onChange={this.handleChange}>
+                                <option value="Pisces">Pisces</option>
+                                <option value="Aries">Aries</option>
+                                <option value="Scorpio">Scorpio</option>
+                                <option value="Gemini">Gemini</option>
+                            </select>
+                            <input type="submit" value="Submit" />
+                        </label>
+                    </form>
+                </div>
                 <div className="footer">
                     Fork this on my github <a href="https://github.com/lillypiri/30apps30days">@lillypiri</a>.
                     <div>
@@ -12,7 +44,7 @@ class Nine extends Component {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
