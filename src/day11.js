e8aaf9d93e8bcab8f15861a09d7ebfe63e1141fa
Lilly Piri <lilly@lillypiri.com>
2017-06-12 11:36:58 +1000

    eggos can be eaten now

--- src/day11.js
+++ src/day11.js
@@ -1,16 +1,81 @@
 import React, {Component} from 'react';
+import eggo from './images/eggo.gif';
 import './day11.css'
 
 
-class Eleven extends Component {
+class Day11 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEaten: false
+        };
+    }
+
     render() {
         return(
             <div>
-                Waffle
+                {!this.state.isEaten &&
+                    <div className="eggo">
+                        Eat the eggos!
+                    </div>
+                }
+                {this.state.isEaten &&
+                    <div className="eggo">
+                        That was delicious!
+                    </div>
+                }
+
+                <div>
+                    <Eggo isEaten={this.state.isEaten} onEaten={isEaten => this.setState({ isEaten })} />
+                </div>
             </div>
         )
 
     }
 }
 
-export default Eleven;
+class Eggo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: []
+        }
+        this.reset = this.reset.bind(this);
+
+        this.eatenEggo = this.eatenEggo.bind(this);
+    }
+
+    reset () {
+        this.setState({
+            items: []
+        })
+        this.props.onEaten(false);
+    }
+
+    eatenEggo(item) {
+        if (this.state.items.length === 0) {
+            this.props.onEaten(true);
+        }
+        this.setState(prevState => ({
+            items: prevState.items.concat(item)
+        }));
+    }
+
+    render () {
+        return (
+            <div className="eatenEggo">
+                {this.state.items.indexOf("eggo") === -1 &&
+                    <img src={eggo} className="item" alt="eggo" onClick={e => this.eatenEggo("eggo")}/>
+                }
+
+                {this.props.isDone &&
+                    <div>
+                        <button className="button" onClick={this.reset}>Play Again!</button>
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default Day11;
