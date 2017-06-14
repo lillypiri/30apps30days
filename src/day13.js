d5be3f4d678585bacb08640651dc57eb3def046c
Lilly Piri <lilly@lillypiri.com>
2017-06-14 14:53:49 +1000

    Added fetch to consume an API

--- src/day13.js
+++ src/day13.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
+import './day13.css'
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-class Thirteen extends Component {
+
+class APOD extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            image: null
+        }
+    }
+
+    componentDidMount () {
+        fetch('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo').then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        }).then(image => {
+            this.setState({
+                image: image
+            })
+        });
+    }
+
     render() {
+        if (!this.state.image) return null;
+
         return (
-            <div>
-                13
+            <div className="apod">
+                <img src={this.state.image.url} alt={this.state.image.title} />
+                <div>
+                    Data provided by <a href="https://api.nasa.gov/">Nasa API</a>.
+                </div>
             </div>
         )
     }
 }
 
-export default Thirteen;
+
+export default APOD;
