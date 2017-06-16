bd014e382a09c730b50e9acf9c82437e4289e62a
Lilly Piri <lilly@lillypiri.com>
2017-06-16 22:48:20 +1000

    testing out searches

--- src/day15.js
+++ src/day15.js
@@ -1,53 +1,55 @@
 import React, {Component} from 'react';
-import './day14.css'
+import './day15.css'
 
 
 class Mystery extends Component {
     constructor(props) {
         super (props);
 
+        this.fetchGif = this.fetchGif.bind(this);
+
         this.state = {
-            isPushed: false,
+            query: '',
             image: null
         };
     }
 
-    componentDidMount () {
-        fetch('http://api.giphy.com/v1/gifs/search?q=funny+cat&limit=1&rating=g&api_key=dc6zaTOxFJmzC').then(function(response) {
+
+    fetchGif (event) {
+        event.preventDefault();
+
+        if (!this.state.query) {
+            alert("Type something in!");
+            return;
+        }
+
+        fetch(`http://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(this.state.query)}&limit=1&rating=g&api_key=dc6zaTOxFJmzC`).then(function(response) {
             if (response.status >= 400) {
                 throw new Error("Bad response from server");
             }
             return response.json();
         }).then(response => {
             this.setState({
-                images: response.data
-            })
+                image: response.data[0]
+            });
         });
     }
 
+
     render() {
-        if (!this.state.images) return null;
-        return(
-
+        return (
             <div className="fifteen">
-                {!this.state.isPushed &&
-                    <div className="mystery" onClick={e => this.setState({ isPushed: true })}>
-                        push me
-                    </div>
-                }
+                <form onSubmit={this.fetchGif}>
+                    <input autoFocus className="fifteen-input" type="text" onChange={e => this.setState({ query: e.target.value })} value={this.state.query} placeholder="Search for a GIF" />
+                    <button type="submit" className="mystery">Find a GIF</button>
+                </form>
 
-                {this.state.isPushed &&
-                    <div className="mysteryreveal">
-                        <div className="gifs">
-                            {this.state.images.map(image => {
-                                return (
-                                    <img key={image.id} src={image.images.downsized.url} alt="" />
-                                )
-                            })}
+                {this.state.image &&
+                    <div className="gifs">
+                        <img key={this.state.image.id} src={this.state.image.images.downsized.url} alt="" />
 
-                            <div>
-                                Trending G-rated gif by <a href="https://github.com/Giphy/GiphyAPI#trending-gifs-endpoint">GiphyAPI</a>.
-                            </div>
+                        <div>
+                            gif supplied by <a href="https://github.com/Giphy/GiphyAPI#trending-gifs-endpoint">GiphyAPI</a>.
                         </div>
                     </div>
                 }
