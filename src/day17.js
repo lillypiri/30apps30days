f0c1b245e3d6ff9d0e617df3aad1cff2052b37b2
Lilly Piri <lilly@lillypiri.com>
2017-06-18 22:34:39 +1000

    works on mobile

--- src/day17.js
+++ src/day17.js
@@ -11,14 +11,22 @@ class TodoApp extends Component {
 
   render() {
     return (
-      <div className="todolist">
-        <div className="to-do-list-title">To Do List:</div>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input autoFocus className="todo-input" type="text" onChange={this.handleChange} value={this.state.text} />
-          <button type="submit" className="add-todo">{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
-      </div>
+        <div>
+          <div className="todolist">
+            <div className="to-do-list-title">To Do List:</div>
+            <TodoList items={this.state.items} />
+            <form onSubmit={this.handleSubmit}>
+              <input autoFocus className="todo-input" type="text" onChange={this.handleChange} value={this.state.text} />
+              <button type="submit" className="add-todo">{'Add #' + (this.state.items.length + 1)}</button>
+            </form>
+          </div>
+          <div className="footer">
+              Fork this on my github <a href="https://github.com/lillypiri/30apps30days">@lillypiri</a>.
+              <div>
+                  Artwork by <a href="https://lillypiri.com">Lilly Piri</a>.
+              </div>
+          </div>
+        </div>
     );
   }
 
