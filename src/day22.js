6c002353e96de2428cd9b04acaf262371f8f24d5
Lilly Piri <lilly@lillypiri.com>
2017-06-23 16:50:32 +1000

    fixed CSS flex styling issues

--- src/day22.js
+++ src/day22.js
@@ -7,7 +7,7 @@ class TextBox extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = {value: 'Type something here!'};
+    this.state = {value: 'Type some Markdown here! Example: `back-ticks`  ~~strikethrough~~ *italics* '};
   }
 
   handleChange(e) {
@@ -21,17 +21,36 @@ class TextBox extends Component {
 
   render() {
     return (
-      <div className="text-box">
-        <h3>Input</h3>
-        <textarea
-          onChange={this.handleChange}
-          defaultValue={this.state.value} />
-        <h3>Output</h3>
-        <div
-          className="content"
-          dangerouslySetInnerHTML={this.getRawMarkup()}
-        />
-      </div>
+        <div>
+            <div className="container">
+                <div className="column">
+                    <div className="text-box">
+                        <div className="title-23">Input</div>
+                        <textarea className="textarea"
+                            onChange={this.handleChange}
+                            defaultValue={this.state.value}
+                            autoFocus />
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="output-box">
+                        <div className="title-23">Output</div>
+                        <div
+                            className="markdown-output"
+                            dangerouslySetInnerHTML={this.getRawMarkup()}
+                            />
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer">
+              <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">Markdown Cheatsheet here</a>.
+              <div>
+                  This app uses the <a href="https://www.npmjs.com/package/remarkable">remarkable package</a>.
+              </div>
+            </div>
+        </div>
     );
   }
 }
